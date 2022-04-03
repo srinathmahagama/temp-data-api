f48ef3aeabad4eb98e0c85851ad84468498cd9d4
Srinath Mahagama <SrinathM@embla.asia>
2022-04-03 08:09:39 +0530

    Uncomment cron job

--- src/sensor-temperature/sensor-temperature/sensor-temperature.service.ts
+++ src/sensor-temperature/sensor-temperature/sensor-temperature.service.ts
@@ -24,18 +24,18 @@ export class SensorTemperatureService {
     }
 
     //This method calls every minute's 15th and 45th second
-    // @Cron('15,45 * * * * *')
-    // async listenToCronJob(): Promise<any> {
-    //     this.logger.debug('Cron job started');
-    //     const cronObject: TemperatureSensorCreateDto = {
-    //         sensorId: '2',
-    //         roomId: Number((Math.random() * 10).toFixed(0)),
-    //         devicePosition: DevicePosition.OUT,
-    //         temperature: Number((Math.random() * 100).toFixed(2)),
-    //     }
-    //     await this.syncToDb(cronObject);
-    //     this.logger.debug('Cron job end');
-    // }
+    @Cron('15,45 * * * * *')
+    async listenToCronJob(): Promise<any> {
+        this.logger.debug('Cron job started');
+        const cronObject: TemperatureSensorCreateDto = {
+            sensorId: '1',
+            roomId: Number((Math.random() * 10).toFixed(0)),
+            devicePosition: DevicePosition.IN,
+            temperature: Number((Math.random() * 100).toFixed(2)),
+        }
+        await this.syncToDb(cronObject);
+        this.logger.debug('Cron job end');
+    }
 
     async loadSensorData(query: TemperatureSensorLoadDto): Promise<TemperatureSensor[]> {
         let fiter = {};
